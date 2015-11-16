ef1a20261477fe8bc8bc530bd5399c91c84a0091
Roman Yurchak <rth@crans.org>
2015-11-16 17:30:16 +0100

    Issue #3: add tests for the sesame file

---
+++ opacplot2/tests/test_sesame.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os.path
+
+import nose
+from numpy.testing import assert_allclose
+import numpy as np
+
+import opacplot2 as opp
+
+
+
+BASE_DIR = os.path.join(os.path.dirname(__file__), 'data')
+reference_name =  'matr_005761.ses'
+
+
+def test_opac_read():
+    fh = opp.OpgSesame(os.path.join(BASE_DIR, reference_name),
+            opp.OpgSesame.SINGLE)
