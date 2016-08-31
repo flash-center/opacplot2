ce063c2728873054145337e196ce38d5baac86c2
JT Laune <jtlaune@gmail.com>
2016-08-31 16:18:21 -0500

    Travis build.

--- opacplot2/tests/run.py
+++ opacplot2/tests/run.py
@@ -7,7 +7,7 @@ import pytest
 _base_dir, _ = os.path.split(__file__)
 
 def run(coverage=False):
-    argv=['', '-x', '{}'.format(_base_dir), '-v']
+    argv=['-x', '{}'.format(_base_dir), '-v']
     if coverage:
         argv += ['--cov-report', 'term-missing', '--cov=opacplot2']
     result = pytest.main(argv)
