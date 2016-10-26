ff3c0580bad9372c9c6b47de8d14020b5caff445
JT Laune <jtlaune@gmail.com>
2016-10-26 14:45:34 -0500

    Update colorbars for opac-error

--- opacplot2/scripts/opac_error.py
+++ opacplot2/scripts/opac_error.py
@@ -720,14 +720,14 @@ def compare_eos(eos_1, eos_2, verbose=False,
             fig, axarr = plt.subplots(1,3)
             x, y = np.meshgrid(d, t)
             res_levels = {0:1, 1:.1, 2:.01 }
-            fig.set_size_inches(18, 6)
+            fig.set_size_inches(19, 6)
                 
             for i in range(3):    
-                levels = np.linspace(0, res_levels[i], 15)
+                levels = np.linspace(0, res_levels[i], 256)
                 cs = axarr[i].contourf(x, y, np.sqrt(err_1_sqr).T, 
                                        levels, extend='max')
                 cb = plt.colorbar(cs, ax=axarr[i])
-                cb.formatter = matplotlib.ticker.FuncFormatter(lambda x,p: '{.2}%'.format(x*100))
+                cb.formatter = matplotlib.ticker.FuncFormatter(lambda x,p: '{:.1f}%'.format(float(x)*100))
                 cb.update_ticks()
                 if i==2:
                     cb.set_label('% Error')
